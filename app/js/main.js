3c4206f97098afccca3a82f3a227cdd5473aac47
Margo-23 <issochka2017@gmail.com>
2021-06-29 19:57:28 +0300

    added categories, benegites, fixed margin for header__bottom 1h+40m

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
+  $('.header__bottom__link').on('click', function(e){
+    $('.header__bottom-menu').toggleClass('header__bottom-menu--active');
+    $('.header__bottom__inner').css( "display", "none" );
+    $('.header__bottom-menu__list').css( "display", "block" );
+    $('.logo').css( "display", "none" );
+    $('.logo-blue').css( "display", "block" );
+    
+  });
 
 });
 
