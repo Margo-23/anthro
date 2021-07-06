90fba5a93dbd6aeca16313756d5386707e441501
Margo-23 <issochka2017@gmail.com>
2021-07-06 20:10:17 +0300

    added page Blog

--- app/js/main.js
+++ app/js/main.js
@@ -4,52 +4,45 @@ $(function(){
     $('.header__bottom__inner').css( "display", "none" );
     $('.header__bottom-menu__list').css( "display", "block" );
     $('.logo').css( "display", "none" );
-    $('.logo-blue').css( "display", "block" );
-    
+    $('.logo-blue').css( "display", "block" );    
   });
-});
 
-$(function(){
   $('.collections__item-name').on('click', function(e){
     e.preventDefault();
     $(this.parentElement).toggleClass('collections__item--active');    
   });
-});
 
-$(function(){
+
   $('.collections__item-submenu__element-link').on('click', function(e){
     e.preventDefault();
     $('.collections__item-submenu__element-link').removeClass('collections__item-submenu__element-link--active');
     $(this).addClass('collections__item-submenu__element-link--active');    
   });
-});
 
-$(function(){
+
+
   $('.catalog__filters__top').on('click', function(e){
     $(this).children('.catalog__filters__btn').toggleClass('catalog__filters__btn--open'); 
     $(this).next('.catalog__filters__form').toggle();   
   });
-});
 
-$(function(){
+
   $('.color__text').on('click', function(e){
     $(this).toggleClass('color__text--active'); 
   });
-});
 
-$(function(){
+
+
   $('.material__text').on('click', function(e){
     $(this).toggleClass('material__text--active'); 
   });
-});
 
-$(function(){
+
   $('.price__text').on('click', function(e){
     $(this).toggleClass('price__text--active'); 
   });
-});
 
-$(function(){
+
   $('.catalog__filters__reset').on('click', function(e){
     $('.color__text').removeClass('color__text--active');
     $('.material__text').removeClass('material__text--active');
@@ -57,13 +50,18 @@ $(function(){
     $('.catalog__filters__btn').removeClass('catalog__filters__btn--open');
     $('.catalog__filters__form').css('display', 'none');
   });
+
+  $('.comment__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.comment__slider-prew'),
+    nextArrow: $('.comment__slider-next'),
+  });
+
+
+  var containerEl = document.querySelector('.blog-page__inner');
+  var mixer = mixitup(containerEl);
+
+
 });
 
-$(function(){
-$('.comment__slider').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow: $('.comment__slider-prew'),
-  nextArrow: $('.comment__slider-next'),
-});
-});
